f94f40b04d2666bf2d1870cffdf51d42b8c26c53
unknown <nazmul@nazmul-PC.(none)>
2012-11-15 23:35:19 +0600

    xml extension is allowed

---
+++ src/general/JavaFilter.java
@@ -0,0 +1,17 @@
+package general;
+import java.io.File;
+
+/**
+ *This class supports only XML file to upload for the application
+ */
+public class JavaFilter extends javax.swing.filechooser.FileFilter
+{
+  public boolean accept (File f) {
+    return f.getName ().toLowerCase ().endsWith (".xml")
+          || f.isDirectory ();
+  }
+
+  public String getDescription () {
+    return "XML Files (*.xml)";
+  }
+}
